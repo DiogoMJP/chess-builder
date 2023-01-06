877c416846c4cfd7e47204aa4d9fd7952dab7965
DiogoMJP <diogo.mj.pereira@gmail.com>
2023-01-06 23:07:07 +0000

    Modified: 	=chess.py: 		-Continued editing the existing classes.

--- chess.py
+++ chess.py
@@ -1,28 +1,69 @@
 from abc import ABC, abstractmethod
 
 
+Vector = tuple[int, int]
+
+
 class Board():
-    def __init__(self, name, width, height):
+    def __init__(self, name: str, size: Vector) -> None:
         self.name = name
-        self.size = (width, height)
+        self.size = size
 
 
 class MovementRule(ABC):
     @abstractmethod
-    def get_movs(self, board, pos):
+    def __init__(self, mov_list: list[Vector], mov_rule: str) -> None:
         pass
 
     @abstractmethod
-    def move(self, board, pos):
+    def can_move(self, board: Board, mov: Vector) -> bool:
+        pass
+
+    @abstractmethod
+    def get_movs(self, board: Board, mov: Vector) -> list[Vector]:
+        pass
+
+    @abstractmethod
+    def move(self, board, pos, target) -> None:
         pass
 
 
 class AbsoluteMovement(MovementRule):
-    def __init__(self, mov_list):
+    def __init__(self, mov_list: list[Vector], mov_rule: str) -> None:
+        self.mov_list = mov_list
+        self.mov_rule = lambda board, mov: exec(mov_rule)
+
+    def can_move(self, board: Board, mov: Vector) -> bool:
+        return self.mov_rule(board, mov)
+    
+    def get_movs(self, board: Board, mov: Vector) -> list[Vector]:
+        movs = []
+        for mov in self.mov_list:
+            if self.can_move(board, mov):
+                movs += [mov]
         
+        return movs
+
+
+class RelativeMovement(MovementRule):
+    def __init__(self, mov_list: list[Vector], mov_rule: str) -> None:
+        self.mov_list = mov_list
+        self.mov_rule = lambda board, mov: exec(mov_rule)
+
+    def can_move(self, board: Board, mov: Vector) -> bool:
+        return self.mov_rule(board, mov)
+    
+    def get_movs(self, board: Board, pos: Vector) -> list[Vector]:
+        movs = []
+        for mov in self.mov_list:
+            if self.can_move(board, mov):
+                movs += [(pos[0] + mov[0], pos[1] + mov[1])]
+        
+        return movs
+
 
 class Piece():
-    def __init__(self, name, image):
+    def __init__(self, name: str, image: str) -> None:
         self.name = name
         self.image = image
         self.mov_rules = []
@@ -30,17 +71,16 @@ class Piece():
         self.board = None
         self.pos = None
 
-    def set_piece(self, board, x, y):
+    def set_piece(self, board: Board, pos: Vector) -> None:
         self.board = board
         self.pos = (x, y)
         self.is_piece = True
 
-    def add_movements(self, mov_list, rule_type):
+    def add_movements(self, mov_list: list[Vector], rule_type: str) -> None:
         if rule_type == "ABS":
             self.mov_rules += [lambda pos: [(pos[0] + mov[0], pos[1] + mov[1]) for mov in mov_list]]
         elif rule_type == "REL":
             self.mov_rules += [lambda pos: [mov for mov in mov_list]]
 
-    def get_valid_movements(self):
-        mov_list = [mov for mov in movs for movs in [mov_rules(self.pos) for mov_rules in self.mov_rules]]
-        print(mov_list)
+    def get_valid_movements(self) -> None:
+        pass
